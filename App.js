510d3bc6b9d496ec6d633e3884c0f2ae416fd20a
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-03 11:33:00 -0400

    Remove state and API fetching

--- App.js
+++ App.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import API from './app/api';
+import Login from './app/components/Login';
+
 
 export default class App extends React.Component {
-
-    constructor() {
-        super();
-        this.state = { records: [] };
-        this.fetchAirtable = this.fetchAirtable.bind(this);
-    }
-
-    async componentDidMount() {
-        await this.fetchAirtable();
-    }
-
-    async fetchAirtable() {
-        const records = await API.get('plates');
-        this.setState({ records });
-    }
-
     render() {
-        const { records } = this.state;
-
         return (
             <View style={styles.container}>
-                {records && records.length > 0 ? records.map((record, i) =>
-                    <Text key={ i }>{ record.fields.plate_number }</Text>
-                ) : <Text>Double-check that you have added your API key to .env.</Text>}
+                <Login />
             </View>
         );
     }
