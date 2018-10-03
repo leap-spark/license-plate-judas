6cf8d6928562a8409d7ea7f135c2a1490644a994
Nick Peay <npeay@thejumpagency.com>
2018-10-03 15:49:07 -0400

    Add nav route to App.js

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Login from './app/components/Login';
+import Lookup from './app/views/home';
+import { createStackNavigation } from 'react-navigation';
 
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Login />
-            </View>
-        );
+// export default class App extends React.Component {
+//     render() {
+//         return (
+//             <View style={styles.container}>
+//                 <Login />
+//             </View>
+//         );
+//     }
+// }
+
+const RootStack = createStackNavigator(
+    {
+      Home: Login,
+      Lookup: Lookup,
+    },
+    {
+      initialRouteName: 'Home',
     }
-}
+);
 
 const styles = StyleSheet.create({
     container: {
@@ -21,3 +33,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 });
+
+export default class App extends Component {
+  render() {
+    return <RootStack />
+  }
+}
