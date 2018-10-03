46f0346c7c453c481273318de582064d5f2239d3
Nick Peay <npeay@thejumpagency.com>
2018-10-03 16:53:10 -0400

    Extract Text Inputs to Lookup component

--- app/views/home/index.js
+++ app/views/home/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Lookup from '../../components/Lookup';
 
 export default class App extends Component {
   render() {
@@ -12,23 +13,7 @@ export default class App extends Component {
 
     return (
         <View style={styles.home}>
-          <TextInput
-              style={styles.input}
-              placeholder="Enter License Plate"
-              maxLength={6}
-          />
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity
-                style={styles.button}
-            >
-              <Text> Lookup </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.ghostBtn}
-            >
-              <Text> Submit </Text>
-            </TouchableOpacity>
-          </View>
+          <Lookup />
         </View>
     )
 
@@ -43,32 +28,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    alignSelf: 'center',
-    width: 200,
-    backgroundColor: '#fff',
-    borderWidth: 0.5,
-    borderColor: '#777',
-    padding: 10,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    margin: 30,
-  },
-  button: {
-    backgroundColor: '#DDDDDD',
-    paddingTop: 12,
-    paddingBottom: 12,
-    paddingLeft: 24,
-    paddingRight: 24,
-  },
-  ghostBtn: {
-    borderWidth: 1,
-    backgroundColor: '#FFFFFF',
-    paddingTop: 12,
-    paddingBottom: 12,
-    paddingLeft: 24,
-    paddingRight: 24,
-  }
 });
