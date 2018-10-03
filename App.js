06432faa25d95f6c340cc4b3629955592a05d850
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-03 11:56:06 -0400

    Remove unused imports

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import API from './app/api';
+import { StyleSheet, View } from 'react-native';
 import Login from './app/components/Login';
 
 
