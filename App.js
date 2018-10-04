416b4c3a6678057d602067052baeb5fcedebe84c
Aaron Arney <aaron.arney@ocular-rhythm.com>
2018-10-03 20:43:55 -0400

    Auto-fix styling issues reported by eslint

--- App.js
+++ App.js
@@ -1,41 +1,20 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
 import Login from './app/components/Login';
-import Lookup from './app/views/home';
-import { createStackNavigation } from 'react-navigation';
-
-
-// export default class App extends React.Component {
-//     render() {
-//         return (
-//             <View style={styles.container}>
-//                 <Login />
-//             </View>
-//         );
-//     }
-// }
+import Home from './app/views/Home';
+import { createStackNavigator } from 'react-navigation';
 
 const RootStack = createStackNavigator(
     {
-      Home: Login,
-      Lookup: Lookup,
+        Home: Login,
+        Lookup: Home,
     },
     {
-      initialRouteName: 'Home',
+        initialRouteName: 'Home',
     }
 );
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
-
 export default class App extends Component {
-  render() {
-    return <RootStack />
-  }
+    render() {
+        return <RootStack />
+    }
 }
--- app/views/Home/index.js
+++ app/views/Home/index.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Lookup from '../../components/Lookup';
 
-export default class App extends Component {
-  render() {
+export default class Home extends Component {
+    render() {
 
-    //TODO: If logged in, continue, else display login
+        //TODO: If logged in, continue, else display login
 
-    //TODO: Display passenger popup if
-      //TODO: i. Location enabled
-      //TODO: ii Movement faster than 10mph
+        //TODO: Display passenger popup if
+        //TODO: i. Location enabled
+        //TODO: ii Movement faster than 10mph
 
-    return (
-        <View style={styles.home}>
-          <Lookup />
-        </View>
-    )
-
-  }
-
+        return (
+            <View style={styles.home}>
+                <Lookup />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  home: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    home: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
