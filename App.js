cc378a413352437f142b5cfb274237608756dbc1
Nick Peay <npeay@thejumpagency.com>
2018-10-04 16:09:27 -0400

    Add header title bar

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import HomeView from './app/views/HomeView';
 import Lookup from './app/components/Lookup';
+import Login from './app/components/Login';
 import { createStackNavigator } from 'react-navigation';
 
 const RootStack = createStackNavigator(
     {
         Home: HomeView,
         Lookup,
+        Login,
     },
     {
         initialRouteName: 'Home',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#AE0010',
+            },
+            headerTintColor: '#fff',
+            headerTitle: 'License Plate Judas',
+        }
     }
 );
 
