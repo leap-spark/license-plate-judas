102bac451887c627108d66b63cfcf2a788534ca9
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-04 15:34:22 -0400

    Refactor private internal methods to use auto binding arrow functions

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, AsyncStorage } from 'react-native';
 import API from '../../api';
 import config from '../../config';
 import hash from 'object-hash';
@@ -7,10 +7,9 @@ import hash from 'object-hash';
 
 export default class Login extends React.Component {
 
+
     constructor(props) {
         super(props);
-
-        this.onSubmit = this.onSubmit.bind(this);
 
         this.state = {
             username: '',
@@ -20,21 +19,35 @@ export default class Login extends React.Component {
     }
 
 
-    async onSubmit() {
+    _onSubmit = async () => {
+        await this._doLogin();
+    };
+
+
+    _doLogin = async () => {
         const password = hash.MD5(this.state.password + config.salt);
         const query = `?fields%5B%5D=username&fields%5B%5D=password&filterByFormula=AND(username%3D%22${this.state.username}%22,password%3D%22${password}%22)`;
         const records = await API.get('users', query);
 
-        if (!records.length) {
-            this.setState({
-                errors: 'Username/Password Incorrect'
-            });
-
-            return null;
+        if (records.length) {
+            await this._loginSuccess(records[0]);
+        } else {
+            await this._loginFail();
         }
+    };
 
+
+    _loginSuccess = async (data) => {
+        await AsyncStorage.setItem('@UserStore:data', JSON.stringify(data));
         this.props.navigation.navigate('HomeView');
-    }
+    };
+
+
+    _loginFail = async () => {
+        await this.setState({
+            errors: 'Username/Password Incorrect'
+        });
+    };
 
 
     render() {
@@ -61,7 +74,7 @@ export default class Login extends React.Component {
                     onChangeText={(text) => this.setState({ errors: undefined, password: text })}
                 />
                 <Button
-                    onPress={this.onSubmit}
+                    onPress={this._onSubmit}
                     title="Login"
                     color="#841584"
                     accessibilityLabel="Login to the app"
