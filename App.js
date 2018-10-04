7f6bc4618915a0c4c4c81ecd7a2a7a1c2a56c5cf
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-04 15:33:57 -0400

    Refactor stack navigator to use correct components

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Login from './app/components/Login';
-import Home from './app/views/Home';
+import HomeView from './app/views/HomeView';
+import Lookup from './app/components/Lookup';
 import { createStackNavigator } from 'react-navigation';
 
 const RootStack = createStackNavigator(
     {
-        Home: Login,
-        Lookup: Home,
+        Home: HomeView,
+        Lookup,
     },
     {
         initialRouteName: 'Home',
