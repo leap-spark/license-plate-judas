408f8d75b80010def5115e001bf78a5e654ba901
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-04 16:06:54 -0400

    Add Login to the stack navigator

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import HomeView from './app/views/HomeView';
 import Lookup from './app/components/Lookup';
+import Login from './app/components/Login';
 import { createStackNavigator } from 'react-navigation';
 
 const RootStack = createStackNavigator(
     {
         Home: HomeView,
         Lookup,
+        Login
     },
     {
         initialRouteName: 'Home',
