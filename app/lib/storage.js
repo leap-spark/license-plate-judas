35eb02b35b6feb58b84ec425b861d82070a31d66
Aaron Arney <aaron.arney@ocular-rhythm.com>
2018-10-05 15:40:36 -0400

    Add Storage helper class

---
+++ app/lib/storage.js
@@ -0,0 +1,47 @@
+import { SecureStore } from 'expo';
+
+export default class Storage {
+
+    /**
+     * Get a value from local storage by key.
+     *
+     * @param key   string  The key to retrieve.
+     * @returns {Promise<boolean>}
+     */
+    static async get(key) {
+        let data = false;
+
+        try {
+            const value = await SecureStore.getItemAsync(key);
+
+            if (value !== null) {
+                data = value;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+
+        return data;
+    }
+
+
+    /**
+     * Store a value by key.
+     *
+     * @param key   string The key to store to.
+     * @param value string The value to store.
+     * @returns {Promise<*|boolean>}
+     */
+    static async set(key, value) {
+        let data = true;
+
+        try {
+            data = await SecureStore.setItemAsync(key, value);
+        } catch (error) {
+            console.error(error);
+            data = false;
+        }
+
+        return data;
+    }
+}
