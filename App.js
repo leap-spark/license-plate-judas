29a22f640bb341a3d6ad69bc5b42762c0caec207
Aaron Arney <aaron.arney@ocular-rhythm.com>
2018-10-05 13:59:30 -0400

    Group imports by type

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { createStackNavigator } from 'react-navigation';
+
 import HomeView from './app/views/HomeView';
 import Lookup from './app/components/Lookup';
 import Login from './app/components/Login';
-import { createStackNavigator } from 'react-navigation';
 
 
 const TopLevelNavigation = createStackNavigator(
