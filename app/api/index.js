e1be513e9c6742ca901476f5519ce9aeea2902c3
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-08 15:59:53 -0400

    Remove API methods in favor of Firebase

--- app/api/index.js
+++ app/api/index.js
@@ -1,89 +1,23 @@
-import config from '../config';
+import * as firebase from 'firebase';
+import {
+    API_KEY,
+    AUTH_DOMAIN,
+    DATABASE_URL,
+    PROJECT_ID,
+    STORAGE_BUCKET,
+    MESSAGING_SENDER_ID
+} from 'react-native-dotenv';
 
 
-/**
- * The API class
- *
- * Encapsulates methods pertaining to performing API requests against Airtable.
- *
- * @since  0.0.1
- * @author Aaron Arney
- */
-export default class API {
+const firebaseConfig = {
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    databaseURL: DATABASE_URL,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID
+};
 
+firebase.initializeApp(firebaseConfig);
 
-    /**
-     * Perform the basic fetch request to Airtable.
-     *
-     * @since 0.0.1
-     *
-     * @param method    string The HTTP method to perform (get, post, update)
-     * @param table     string The Airtable table to perform the action on
-     * @param data      object An object of fields and values
-     * @param params    array  Additional params to pass with the request
-     * @returns {Promise<*>}
-     */
-    static async doRequest(method = this.isRequired(), table = this.isRequired(), data, params = '') {
-
-        const headers = {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${config.apiKey}`,
-        };
-
-        // TODO: Do better validation here
-        if (data !== undefined || data !== {}) {
-            headers.fields = data;
-        }
-
-        try {
-            const response = await fetch(`https://api.airtable.com/v0/${config.base}/${table}${params}`, {
-                method,
-                headers,
-            });
-
-            return await response.json();
-        } catch (error) {
-            return [];
-        }
-    }
-
-
-    static isRequired() {
-        throw new Error('Parameter required when invoking API method');
-    }
-
-
-    /**
-     * Executes a GET request.
-     *
-     * @since 0.0.1
-     *
-     * @param table  string The Airtable table to perform the action on
-     * @param params string A string containing additional params
-     * @returns {Promise<void>}
-     */
-    static async get(table = this.isRequired(), params) {
-        const response = await this.doRequest('GET', table, {}, params);
-        const { records } = await response;
-
-        return records;
-    }
-
-
-    /**
-     * Executes a POST request.
-     *
-     * @since 0.0.1
-     *
-     * @param table string The Airtable table to perform the action on
-     * @param data  string An object containing fields and values
-     * @returns {Promise<void>}
-     */
-    static async post(table = this.isRequired(), data = this.isRequired(), params) {
-        const response = await this.doRequest('POST', table, data, params);
-        const { records } = await response;
-
-        return records;
-    }
-}
+export default firebase;
