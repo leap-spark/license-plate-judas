d86a4504fb3934c8b865c0998d97d414253acc7b
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-09 13:07:12 -0400

    Version bump to 0.1.1

--- app/config/index.js
+++ app/config/index.js
@@ -1,15 +1,1 @@
-import {
-    AIRTABLE_BASE,
-    AIRTABLE_VIEW,
-    AIRTABLE_API_KEY,
-    AIRTABLE_MAX_RECORDS,
-    SALT
-} from 'react-native-dotenv';
 
-export default {
-    base: AIRTABLE_BASE,
-    view: AIRTABLE_VIEW,
-    apiKey: AIRTABLE_API_KEY,
-    maxRecords: AIRTABLE_MAX_RECORDS,
-    salt: SALT,
-};
