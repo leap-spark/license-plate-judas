b750334f5d06d17f2dbad3e2871cc94446d2f5aa
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-10 15:59:04 -0400

    Complete the _doReportSubmission method

--- app/components/Lookup/index.js
+++ app/components/Lookup/index.js
@@ -16,26 +16,76 @@ export default class Lookup extends Component {
     }
     
     
+    _doReportSubmission = async () => {
+
+        // TODO: We need to do some sanitation checks on the state.plate
+        // TODO: Improve the error handling
+
+        /**
+         * [1] Generate a new key in the reports tree
+         * [2] Grab the current users UID
+         *
+         * Next, we need to start collecting the updates that are going to be made to the tree.
+         * [3] First we need to store the plate as an index in the offenders tree by setting it to true
+         * [4] Next the report key should be appended to the users_meta under the reports_submitted child
+         * [5] Lastly, in the offenders_meta tree, using the plate as an index, append the report_key child
+         *
+         * [6] Run the update
+         * [7] Return the key so that the next view can know which report to update
+         */
+        const REPORT_KEY = firebase.database().ref('/reports').push().key; // [1]
+        const userID = await firebase.auth().currentUser.uid; // [2]
+
+        let updates = {};
+        updates['/offenders/' + this.state.plate] = true; // [3]
+
+        // For debugging purposes
+        // updates['/reports/' + REPORT_KEY] = {
+        //     plate_number: this.state.plate,
+        //     mood: false,
+        //     reason: false,
+        //     reported_by: userID,
+        //     location: false,
+        //     timestamp: new Date()
+        // };
+
+        updates['/users_meta/' + userID + '/reports_submitted/' + REPORT_KEY] = true; // [4]
+        updates['/offenders_meta/' + this.state.plate  + '/associated_reports/' + REPORT_KEY] = true; // [5]
+
+        await firebase.database().ref().update(updates).catch((error) => console.log(error)); // [6]
+
+        // [7]
+        return await this.setState({
+            key: REPORT_KEY
+        });
+    };
+
+
     render() {
         return (
             <View>
                 <TextInput
-                    style={styles.input}
+                    style={ styles.input }
                     placeholder="Enter License Plate"
-                    maxLength={6}
+                    maxLength={ 6 }
+                    autoCapitalize={ true }
+                    required={ true }
+                    autoFocus={ true }
+                    onChangeText={ (plate) => this.setState({ plate }) }
                 />
                 <View style={styles.buttonContainer}>
-                    <TouchableOpacity
+                    <Button
+                        onPress={ this._doSearch }
+                        title="Search"
+                        accessibilityLabel="Search"
                         style={styles.button}
-                        onPress={() => this.props.navigator.navigate('Login')}
-                    >
-                        <Text> Lookup </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={styles.ghostBtn}
-                    >
-                        <Text> Submit </Text>
-                    </TouchableOpacity>
+                    />
+                    <Button
+                        onPress={ this._doReportSubmission }
+                        title="Report"
+                        style={ styles.ghostBtn }
+                        accessibilityLabel="Report"
+                    />
                 </View>
             </View>
         );
