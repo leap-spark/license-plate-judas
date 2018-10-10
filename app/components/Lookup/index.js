f525958dd58e29fc4cfdd0f04b03b2bc61bf1c26
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-10 16:02:57 -0400

    Add comment

--- app/components/Lookup/index.js
+++ app/components/Lookup/index.js
@@ -23,6 +23,7 @@ export default class Lookup extends Component {
             return null;
         }
 
+        // TODO: Do something with this data. Should probably move this method into the Lookup Details view/components
         return await firebase.database().ref('/offenders_meta').child(this.state.plate + '/associated_reports').once('value')
             .then((snapshot) => snapshot.val());
     };
