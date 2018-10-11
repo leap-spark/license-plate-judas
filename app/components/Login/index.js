d124d161725ec0698da1160db41034b87a6e7416
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-11 13:53:04 -0400

    Remove registration methods and related logic

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -47,29 +47,11 @@ export default class Login extends Component {
 
         await firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(this._handleErrorMessage);
 
-        if (firebase.auth().currentUser) {
-            await this._loginSuccess();
-        }
-
         this._endActivityIndicators();
-    };
-
-
-    _doRegistration = async () => {
-        this._startActivityIndicators();
-
-        if (this.state.password !== this.state.confirmPassword) {
-            alert('Passwords must match');
-            return;
-        }
-
-        await firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch(this._handleErrorMessage);
 
         if (firebase.auth().currentUser) {
             await this._loginSuccess();
         }
-
-        this._endActivityIndicators();
     };
 
 
@@ -110,71 +92,15 @@ export default class Login extends Component {
     };
 
 
-    _renderRegisterFields = () => {
-        return (
-            <View>
-                <TextInput
-                    style={{ height: 40, width: 300 }}
-                    placeholder="Email"
-                    autoCapitalize="none"
-                    required={ true }
-                    autoFocus={ true }
-                    onChangeText={(text) => this.setState({ errors: undefined, email: text })}
-                />
-                <TextInput
-                    style={{ height: 40, width: 300 }}
-                    placeholder="Password"
-                    required={ true }
-                    autoCapitalize="none"
-                    onChangeText={(text) => this.setState({ errors: undefined, password: text })}
-                />
-                <TextInput
-                    style={{ height: 40, width: 300 }}
-                    placeholder="Repeat Password"
-                    required={ true }
-                    autoCapitalize="none"
-                    onChangeText={(text) => this.setState({ errors: undefined, confirmPassword: text })}
-                />
-                <Button
-                    onPress={this._doRegistration}
-                    title="Register"
-                    color="#841584"
-                    accessibilityLabel="Register"
-                />
-            </View>
-        );
-    };
-
-
     render() {
         // TODO: Fix this so its floating in the center of screen or something
         const loadingIndicator = this.state.isDoingAction ? (<ActivityIndicator size="large" color="#0000ff" />) : null;
-        let elements = null;
-        let formAction = '';
-
-        if (this.state.formAction === 'login') {
-            elements = this._renderLoginFields();
-            formAction = 'register';
-        } else if (this.state.formAction === 'register') {
-            elements = this._renderRegisterFields();
-            formAction = 'login';
-        }
-
-        const alternateTheFormAction = (
-            <Button
-                onPress={ () => this.setState({ formAction })}
-                title={ formAction.toUpperCase() }
-                color="tomato"
-            />
-        );
 
         return (
             <View>
                 { loadingIndicator }
 
-                { elements }
-
-                { alternateTheFormAction }
+                { this._renderLoginFields() }
             </View>
         );
     }
