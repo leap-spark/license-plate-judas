6f59b1746e7dab445f7781d8d07ae793ac8a5e76
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-11 11:42:04 -0400

    Add LookupDetail component

---
+++ app/components/LookupDetail/index.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+import firebase from '../../api';
+
+
+export default class LookupDetail extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            reports: []
+        };
+    }
+
+
+    async componentDidMount() {
+        await this._getReports();
+    }
+
+
+    _getReports = async () => {
+        const plate = this.props.navigator.getParam('plate');
+        const reportsNormalized = [];
+
+        await firebase.database().ref('/reports')
+            .orderByChild('plate_number')
+            .equalTo(plate.toLowerCase())
+            .once('value', (snapshot) => {
+                snapshot.forEach((childSnapshot) => {
+                    reportsNormalized.push({
+                        ...childSnapshot.val()
+                    });
+                });
+            })
+            .catch((error) => console.error(error));
+
+        await this.setState({
+            reports: reportsNormalized
+        })
+    };
+
+
+    render() {
+        return (
+            <View>
+                <Text>Details</Text>
+                { this.state.reports.map((i, j) => {
+                    return (
+                        <View key={j}>
+                            <Text>{i.plate_number}</Text>
+                            <Text>{i.mood}</Text>
+                            <Text>{i.reason}</Text>
+                            <Text>{i.reported_by}</Text>
+                            <Text>{i.location}</Text>
+                            <Text>{i.timestamp}</Text>
+                        </View>
+                    );
+                })}
+            </View>
+        );
+    }
+}
