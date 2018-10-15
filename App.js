12dd6a4f4bc041e4c07bf56caaf3a6e4c716dfac
Aaron Arney <aaron.arney@ocular-rhythm.com>
2018-10-15 13:31:15 -0400

    Add Mood view to the app stack

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
 import LookupView from './app/views/LookupView';
 import LookupDetailView from './app/views/LookupDetailView';
 import LoginView from './app/views/LoginView';
+import MoodView from './app/views/MoodView';
 import RegisterView from './app/views/RegisterView';
 import Authenticator from './app/components/Authenticator';
 
@@ -13,6 +14,7 @@ const AppStack = createStackNavigator(
         Home: LookupView,
         Lookup: LookupView,
         LookupDetail: LookupDetailView,
+        Mood: MoodView,
     },
     {
         navigationOptions: {
