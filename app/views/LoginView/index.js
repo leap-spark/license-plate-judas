9afea227a7c7d4f9c8acd53791b5e2b0d375d0ca
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-17 08:59:54 -0400

    Implement react-paper components

--- app/views/LoginView/index.js
+++ app/views/LoginView/index.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Login from '../../components/Login';
 
 
 // TODO: Needs styling
 export default (props) => (
-    <View style={styles.home}>
+    <View style={ styles.home }>
         <Login navigation={ props.navigation } />
-
-        <TouchableOpacity onPress={ () => props.navigation.navigate('Register') }>
-            <Text>
-                Register
-            </Text>
-        </TouchableOpacity>
     </View>
 );
 
 const styles = StyleSheet.create({
     home: {
-        flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
+        backgroundColor: '#fff',
+        flex: 1,
+        justifyContent: 'center'
     },
 });
