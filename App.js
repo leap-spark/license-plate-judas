080f274d17f2ca5787a58380c8ee36932881f319
Aaron Arney <aarney@leapsparkagency.com>
2018-10-22 09:17:26 -0400

    Replace Tab Navigator with Drawer Navigator

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator, createSwitchNavigator, createDrawerNavigator } from 'react-navigation';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import LookupView from './app/views/LookupView';
@@ -14,8 +13,29 @@ import RegisterView from './app/views/RegisterView';
 import AccountView from './app/views/AccountView';
 
 import Authenticator from './app/components/Authenticator';
+import NavigationDrawer from './app/components/NavigationDrawer';
 
 
+/**
+ * The react navigation works by creating "stacks" of navigation items that get encapsulated into a navigation component.
+ *
+ * [1] First there is the main App stack, which are components that belong to the flow of using the plate lookup/reporting functionality. All were doing
+ *     is saying "here are all the components that are navigable between each other." We can also dictate which view displays first, which is the "Lookup" view.
+ *
+ * [2] Next, we create the drawer navigation - the nav that appears when you click the "..." button in the header. This stack generates drawer items for each
+ *     component passed to it. In this case, we don't want all of the items available in the first stack as their own navigable items, instead we only want
+ *     to allow the user to navigate to two screens - the Lookup and My Account.
+ *
+ * [3] The auth stack exists to enable a user flow between logging in or registering, and the main app. We don't want users to be able to accidentally
+ *     to see any components or a "flash" of content before being redirected back to the login screen. The auth stack exists solely to ensure the login
+ *     view displays first, and after logging in - prevents users from hitting the back button and landing again on the login page. All we need to do here
+ *     is pass the components that are related to login, in this case "LoginView" and "RegisterView".
+ *
+ * [4] Finally, we create a switch navigator, which is essential when using the auth stack. Here we pass a component "Authenticator" to be used as the
+ *     AuthLoading view, DrawerStack to App, and Auth to AuthStack. We then set the initial route to be directed to the AuthLoading component.
+ */
+
+// [1]
 const AppStack = createStackNavigator(
     {
         Lookup: LookupView,
@@ -30,28 +50,33 @@ const AppStack = createStackNavigator(
     }
 );
 
-const TabStack = createMaterialBottomTabNavigator({
-    Home: {
-        screen: AppStack,
-        navigationOptions: {
-            title: 'Lookup',
-            tabBarIcon: (<Icon name="search" size={20} color="#fff" />)
+// [2]
+const DrawerStack = createDrawerNavigator(
+    {
+        Home: {
+            screen: AppStack,
+            navigationOptions: {
+                drawerLabel: 'Lookup',
+                drawerIcon: (<Icon name="search" size={20} color="#000" />)
+            }
+        },
+        MyAccount: {
+            screen: AccountView,
+            navigationOptions: {
+                drawerLabel: 'My Account',
+                drawerIcon: (<Icon name="account-box" size={20} color="#000" />)
+            }
         }
-    },
-    MyAccount: {
-        screen: AccountView,
-        navigationOptions: {
-            title: 'My Account',
-            tabBarIcon: (<Icon name="account-box" size={20} color="#fff" />)
-        }
+    }, {
+        initialRouteName: 'Home',
+        activeColor: '#f0edf6',
+        inactiveColor: '#3e2465',
+        barStyle: { backgroundColor: '#694fad' },
+        contentComponent: NavigationDrawer,
     }
-}, {
-    initialRouteName: 'Home',
-    activeColor: '#f0edf6',
-    inactiveColor: '#3e2465',
-    barStyle: { backgroundColor: '#694fad' },
-});
+);
 
+// [3]
 const AuthStack = createStackNavigator(
     {
         Login: LoginView,
@@ -62,10 +87,11 @@ const AuthStack = createStackNavigator(
     }
 );
 
+// [4]
 const TopLevelNavigation = createSwitchNavigator(
     {
         AuthLoading: Authenticator,
-        App: TabStack,
+        App: DrawerStack,
         Auth: AuthStack,
     },
     {
