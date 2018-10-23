a7332c6cff5c1d2a8a0d97f9859a487fea0a7ad4
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-23 16:25:46 -0400

    Add Config

---
+++ app/config/index.js
@@ -0,0 +1,103 @@
+
+export const reasonsDictionary = {
+    happy: [
+        {
+            'name': 'Waved Me',
+            'id': 'waved_me',
+            'color': '',
+            'icon': 'pan-tool'
+        },
+        {
+            'name': 'Doesn\'t Speed',
+            'id': 'doesnt_speed',
+            'color': '',
+            'icon': 'child-friendly'
+        },
+        {
+            'name': 'Uses Blinkers',
+            'id': 'uses_blinkers',
+            'color': '',
+            'icon': 'wb-iridescent'
+        },
+        {
+            'name': 'Gave Me Spot',
+            'id': 'gave_me_spot',
+            'color': '',
+            'icon': 'input'
+        },
+        {
+            'name': 'Stopped For Bus',
+            'id': 'stopped_bus',
+            'color': '',
+            'icon': 'directions-bus'
+        },
+        {
+            'name': 'Something',
+            'id': 'something_1',
+            'color': '',
+            'icon': 'card-travel'
+        },
+        {
+            'name': 'Something',
+            'id': 'something_2',
+            'color': '',
+            'icon': 'cached'
+        },
+        {
+            'name': 'Something',
+            'id': 'something_3',
+            'color': '',
+            'icon': 'build'
+        }
+    ],
+    angry: [
+        {
+            'name': 'Too Slow',
+            'id': 'slow',
+            'color': '',
+            'icon': 'whatshot'
+        },
+        {
+            'name': 'Too Fast',
+            'id': 'speeds',
+            'color': '',
+            'icon': 'flight'
+        },
+        {
+            'name': 'No Blinkers',
+            'id': 'no_blinkers',
+            'color': '',
+            'icon': 'wb-iridescent'
+        },
+        {
+            'name': 'Cut Off',
+            'id': 'cut_off',
+            'color': '',
+            'icon': 'swap-calls'
+        },
+        {
+            'name': 'Ran Red Light',
+            'id': 'red_light',
+            'color': '',
+            'icon': 'traffic'
+        },
+        {
+            'name': 'Inconsiderate',
+            'id': 'inconsiderate',
+            'color': '',
+            'icon': 'thumb-down'
+        },
+        {
+            'name': 'Didn\'t Wave',
+            'id': 'didnt_wave',
+            'color': '',
+            'icon': 'pan-tool'
+        },
+        {
+            'name': 'Flipped Me Off',
+            'id': 'flipped_me_off',
+            'color': '',
+            'icon': 'touch-app'
+        }
+    ]
+};
