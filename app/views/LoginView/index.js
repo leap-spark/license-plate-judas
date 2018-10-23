2212bec30ae07b4c094f45ad342dd7c68d2db099
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-23 09:15:41 -0400

    Convert component to stateless

--- app/views/LoginView/index.js
+++ app/views/LoginView/index.js
@@ -6,31 +6,11 @@ import Wrapper from '../../components/Wrapper';
 
 
 // TODO: Needs styling
-export default class LoginView extends Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-
-    componentWillMount() {
-        this.props.navigation.setParams({
-            backEnabled: false,
-            title: 'Login'
-        });
-    }
-
-
-    render() {
-        return (
-            <Wrapper>
-                <View style={ styles.home }>
-                    <Login navigation={ this.props.navigation } />
-                </View>
-            </Wrapper>
-        );
-    }
-}
+export default (props) => (
+    <View style={styles.home}>
+        <Login navigation={props.navigation} />
+    </View>
+);
 
 const styles = StyleSheet.create({
     home: {
