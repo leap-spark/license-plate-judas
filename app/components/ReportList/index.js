1d7d6424c36b2f160c4066cf872de28217e12d7f
Aaron Arney <aaron.g.arney@gmail.com>
2018-10-26 10:48:03 -0400

    Add ReportList component

---
+++ app/components/ReportList/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { List } from 'react-native-paper';
+import { FlatList } from 'react-native';
+
+
+export default class ReportList extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    _listItemTemplate = (item) => {
+        const mood = item.mood === 'happy' ? 'mood' : 'mood-bad';
+
+        return (
+            <List.Item
+                key={item.uuid}
+                title={item.reason}
+                description={item.location + ' @ ' + item.timestamp}
+                left={ () => <List.Icon icon={mood} color="#000" /> }
+            />
+        );
+    };
+
+
+    render() {
+        return (
+            <FlatList {...this.props}
+                ListHeaderComponent={<List.Section title="Latest Reports"/>}
+                ListFooterComponent={<List.Section/>}
+                renderItem={({ item }) => this._listItemTemplate(item)} />
+        );
+    }
+}
